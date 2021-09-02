1f4df9a2ca0c8afb248d04bb78f5d22a01062719
JGore99 <jefgore@gmail.com>
2021-09-02 17:41:43 -0400

    pushing changes for EOD Thursday

--- src/components/DrumMachineTopLevel.js
+++ src/components/DrumMachineTopLevel.js
@@ -18,60 +18,8 @@ export default class DrumMachineTopLevel extends React.Component {
     ],
 
     measureGrid: [
-      {
-        kick01: false,
-        kick02: false,
-        kick03: false,
-        kick04: false,
-        kick05: false,
-        kick06: false,
-        kick07: false,
-        kick08: false,
-        kick09: false,
-        kick10: false,
-        kick11: false,
-        kick12: false,
-        kick13: false,
-        kick14: false,
-        kick15: false,
-        kick16: false,
-      },
-      {
-        snare01: false,
-        snare02: false,
-        snare03: false,
-        snare04: false,
-        snare05: false,
-        snare06: false,
-        snare07: false,
-        snare08: false,
-        snare09: false,
-        snare10: false,
-        snare11: false,
-        snare12: false,
-        snare13: false,
-        snare14: false,
-        snare15: false,
-        snare16: false,
-      },
-      {
-        openHat01: false,
-        openHat02: false,
-        openHat03: false,
-        openHat04: false,
-        openHat05: false,
-        openHat06: false,
-        openHat07: false,
-        openHat08: false,
-        openHat09: false,
-        openHat10: false,
-        openHat11: false,
-        openHat12: false,
-        openHat13: false,
-        openHat14: false,
-        openHat15: false,
-        openHat16: false,
-      },
+      { row: { "01": false, "02": false, "03": false } },
+      { row: { "01": false, "02": false, "03": false } },
     ],
   };
 
@@ -86,10 +34,8 @@ export default class DrumMachineTopLevel extends React.Component {
             ))}
           </div>
           <div className="measureRow">
-            {this.state.measureGrid.map(({ measureRow }, i) =>
-              measureRow.map(({ measure }, j) => (
-                <MeasureButton key={j} measure={measure} />
-              ))
+            {this.state.measureGrid.map((row) =>
+              row.map((measure) => <MeasureButton measure={measure} />)
             )}
           </div>
         </div>
--- src/components/MeasureButton.js
+++ src/components/MeasureButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const MeasureButton = (props) => {
-  //const { measureRow } = props.measureRow;
+  //const { measure } = props.measure;
 
   return (
     <button
-      key={props.measure}
-      value={props.measure}
+      //key={props.measure}
+      //value={props.measure}
       onClick={(e) => console.log(props.measure)}
     ></button>
   );
